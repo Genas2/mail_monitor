2425f6f459e4cf009dac372747b66f427847dace
Genas2 <Gennas2@gmail.com>
2011-02-19 09:05:45 +0200

    Added nagios mails listing into main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python2
 
-import imaplib
-import email
+import imaplib, email
+import re, sys
 from connect import *
 
 Mailbox = imaplib.IMAP4(imap_server)
 
 # Init IMAP connection
-Mailbox.login(username, password)
+try:
+    Mailbox.login(username, password)
+except:
+    sys.exc_info()
+    raise
+
 Mailbox.select()
 
 res, messages = Mailbox.search(None, 'Subject', '**')
 
-# Close IMAP connection
-Mailbox.close()
+try:
+    for num in messages[0].split():
+        res, message = Mailbox.fetch(num, '(RFC822)')
+        message_body = message[0][1]
+    
+        mail = email.message_from_string(message_body)
+        subject = mail['Subject']
+
+        if re.match('\*\*[ ]+PROBLEM', subject):
+            print(mail['Subject'])
+ 
+except (KeyboardInterrupt, SystemExit):
+    # Close IMAP connection
+    Mailbox.close()
+    raise
